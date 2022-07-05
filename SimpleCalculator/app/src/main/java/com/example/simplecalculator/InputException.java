938a5ce3a6ccfc5a2a68e3453ff0511b9c169dac
Maleesha <maleeshaashenjvc@gmail.com>
2022-07-05 14:27:38 -0700

    updated

---
+++ SimpleCalculator/app/src/main/java/com/example/simplecalculator/InputException.java
@@ -0,0 +1,14 @@
+package com.example.simplecalculator;
+
+import android.widget.EditText;
+
+public class InputException extends Exception{
+
+    public InputException(String error){
+        super(error);
+
+
+
+    }
+
+}
