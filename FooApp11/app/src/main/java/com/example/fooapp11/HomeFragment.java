71a55d2ac163f64da8191e4e72c5e1d718e0b78d
Ashen Maleesha Fernando <108730840+azaynm@users.noreply.github.com>
2022-10-13 14:15:39 +0530

    added review, product

--- FooApp11/app/src/main/java/com/example/fooapp11/HomeFragment.java
+++ FooApp11/app/src/main/java/com/example/fooapp11/HomeFragment.java
@@ -3,23 +3,90 @@ package com.example.fooapp11;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.fooapp11.adapters.ProductAdapter;
+import com.example.fooapp11.model.Product;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
+
+    ListView productsLV;
+    ArrayList<Product> productModalArrayList;
+    FirebaseFirestore db;
+    private FirebaseAuth mAuth;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        mAuth = FirebaseAuth.getInstance();
+        View contentView = inflater.inflate(R.layout.fragment_home, container, false);
+        productsLV = contentView.findViewById(R.id.products);
+        productModalArrayList = new ArrayList<>();
+        db = FirebaseFirestore.getInstance();
+        loadDatainListview();
+        return contentView;
     }
 
+    private void loadDatainListview() {
+        db.collection("Products").get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        // after getting the data we are calling on success method
+                        // and inside this method we are checking if the received
+                        // query snapshot is empty or not.
+                        if (!queryDocumentSnapshots.isEmpty()) {
+                            // if the snapshot is not empty we are hiding
+                            // our progress bar and adding our data in a list.
+                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                            for (DocumentSnapshot d : list) {
+                                // after getting this list we are passing
+                                // that list to our object class.
+                                Product dataModal = d.toObject(Product.class);
+
+                                // after getting data from Firebase we are
+                                // storing that data in our array list
+                                productModalArrayList.add(dataModal);
+                            }
+                            // after that we are passing our array list to our adapter class.
+                            ProductAdapter adapter = new ProductAdapter(getContext(), productModalArrayList);
+
+                            // after passing this array list to our adapter
+                            // class we are setting our adapter to our list view.
+                            productsLV.setAdapter(adapter);
+                        } else {
+                            // if the snapshot is empty we are displaying a toast message.
+                            Toast.makeText(getContext(), "No data found in Database", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // we are displaying a toast message
+                        // when we get any error from Firebase.
+                        Toast.makeText(getContext(), "Fail to load data..", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 
 
 }
---
+++ FooApp11/app/src/main/java/com/example/fooapp11/model/Product.java
@@ -0,0 +1,29 @@
+package com.example.fooapp11.model;
+
+public class Product {
+    private String name;
+    private String imgUrl;
+
+    public Product() {
+    }
+    public Product(String name, String imgUrl) {
+        this.name = name;
+        this.imgUrl = imgUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
