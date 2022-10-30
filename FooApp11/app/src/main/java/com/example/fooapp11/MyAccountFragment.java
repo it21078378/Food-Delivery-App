190181b2ce50474b4165841f980af76eca7072c0
Thamasha Bandara <110017491+thamasha24@users.noreply.github.com>
2022-10-31 00:14:36 +0530

    updatedVersion

--- FooApp11/app/src/main/java/com/example/fooapp11/MyAccountFragment.java
+++ FooApp11/app/src/main/java/com/example/fooapp11/MyAccountFragment.java
@@ -12,17 +12,36 @@ import android.widget.Button;
 
 
 public class MyAccountFragment extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-
-        return inflater.inflate(R.layout.fragment_my_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_account, container, false);
+        Button startActivity = (Button) view.findViewById(R.id.paymentHISTORY);
+        Button startActivity2 = (Button) view.findViewById(R.id.add);
+        startActivity2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity2();
+            }
+        });
+        startActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity();
+            }
+        });
+        return view;
     }
 
+    private void startActivity2() {
+        Intent i = new Intent(getActivity(), AddProduct.class);
+        startActivity(i);
+    }
 
-
+    private void startActivity() {
+        Intent i = new Intent(getActivity(), PaymentHistory.class);
+        startActivity(i);
+    }
 
 }
